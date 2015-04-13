dd1c6df9e38adbc5dbdf2b61a54fa50a32665f6f
Chris Makler <chris.makler@gmail.com>
2015-04-13 12:50:40 -0700

    removing v1

--- app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('kineticGraphs.version', [
-  'kineticGraphs.version.interpolate-filter',
-  'kineticGraphs.version.version-directive'
-])
-
-.value('version', '0.1');
