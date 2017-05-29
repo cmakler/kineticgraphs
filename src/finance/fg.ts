4cbc3351b6688ed60b83733ceed2decae1387739
Chris Makler <chris.makler@gmail.com>
2017-05-29 10:48:03 -0700

    Starting from scratch

--- src/finance/fg.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../kg.ts"/>
-/// <reference path="capm/asset.ts"/>
-/// <reference path="capm/portfolio.ts"/>
--- src/physics/pg.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="../kg.ts"/>
-/// <reference path="movement/acceleration.ts"/>
